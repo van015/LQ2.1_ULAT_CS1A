c416c6c7f4d10f5bca54627b8ec4a1a306236bab
van015 <ulatvanremlee@gmail.com>
2025-04-04 13:42:58 +0800

    Add files via upload

---
+++ LQ2.1_ULAT_CS1A.js
@@ -0,0 +1,8 @@
+let username = prompt("Please enter your username:");
+let userpassword = prompt("Please enter your password:");
+
+if (username === "haylie" && userpassword === "haylie21") {
+  alert('Welcome ${username}');
+} else {
+  alert('Maybe Username or Password is Invalid or Does not Match')
+}
